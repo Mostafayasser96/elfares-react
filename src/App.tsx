a3a458c88404585b697a3e37fbf2e6a5756d6f07
mostafa <my4055379@gmail.com>
2024-08-26 00:19:02 +0300

    making some changes to src folder, adding typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div> this is the first div</div>
+    </div>
+
+  );
+};
+
+export default App;
