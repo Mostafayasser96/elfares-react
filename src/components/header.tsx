807588495abe73490328a0d13ed0a10c5ee4eacb
mostafa <my4055379@gmail.com>
2024-08-28 02:27:38 +0300

    made some modifications to Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,6 @@ import React from "react";
 const Header = () => {
 
     return (
-    <div className="">
         <div className="">
             <div className="">
                 <img decoding="async" width="1024" height="565"
@@ -12,86 +11,50 @@ const Header = () => {
                     srcSet="https://elfaresservices.com/wp-content/uploads/2024/05/Image-2-1024x565.png 1024w, https://elfaresservices.com/wp-content/uploads/2024/05/Image-2-300x166.png 300w, https://elfaresservices.com/wp-content/uploads/2024/05/Image-2-768x424.png 768w, https://elfaresservices.com/wp-content/uploads/2024/05/Image-2.png 1080w"
                     sizes="(max-width: 1024px) 100vw, 1024px" />
             </div>
-        </div>
-        <div className="">
             <div className="">
                 <h5 className="">ابحث عن السعادة في منزل نظيف
                 </h5>
             </div>
-        </div>
-        <div className="">
-            <h6 className="">مع خدمات التنظيف المهنية لدينا
-            </h6>
-        </div>
-        <div className="">
             <div className="">
-                <section className="">
-                    <div className="">
-                        <div className="">
-                            <div className="">
-                                <div className="">
-                                    <i aria-hidden="true" className="far fa-clock"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="">
-                            <div className="">
-                                <div className="">
-
-                                    <h5 className="">7:00 صباحًا إلى
-                                        8:00 مساءً
-                                    </h5>
-                                </div>
-                                <div className="">
-                                    <div className="">
-                                        <h5 className="">طول الأسبوع</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <h6 className="">مع خدمات التنظيف المهنية لدينا
+                </h6>
             </div>
-        </div>
-        <div className="">
-            <section className="">
+            <div className="">
+                <div className="">
+                    <i aria-hidden="true" className="far fa-clock"></i>
+                </div>
                 <div className="">
                     <div className="">
-                        <div className="">
-                            <div className="">
-                                <i aria-hidden="true" className="fas fa-phone-alt"></i>
-                            </div>
-                        </div>
-
+                        <h5 className="">7:00 صباحًا إلى
+                            8:00 مساءً
+                        </h5>
                     </div>
                     <div className="">
-
-                        <div className="">
-                            <h5 className="">اتصل بنا على:
-                            </h5>
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="">
-                            <h5 className="">0198 415 53 966+
-                            </h5>
-                        </div>
+                        <h5 className="">طول الأسبوع</h5>
                     </div>
                 </div>
-            </section>
-        </div>
-        <div className="">
+            </div>
             <div className="">
-               <div className="">
-                    <a className="" href="https://elfaresservices.com/index.php/ar/contact-_ar/">            
-                            <span className="">احجز الان</span>
+                <div className="">
+                    <i aria-hidden="true" className="fas fa-phone-alt"></i>
+                </div>
+                <div className="">
+                    <h5 className="">اتصل بنا على:
+                    </h5>
+                </div>
+                <div className="">
+                    <h5 className="">0198 415 53 966+
+                    </h5>
+                </div>
+            </div>
+            <div className="">
+                <div className="">
+                    <a className="" href="https://elfaresservices.com/index.php/ar/contact-_ar/">
+                        <span className="">احجز الان</span>
                     </a>
                 </div>
             </div>
-    </div>
-    </div>
-    </div>
-
+        </div>
     )
 };
 export default Header;
